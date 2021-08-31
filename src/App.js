6c581b8eee2a24d1a2ce6db9b5546b6c3fad6f61
Jason Zhu <jzhu43@ucmerced.edu>
2021-08-30 20:45:44 -0700

    Website done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
+//Import Pages
+import AboutMe from "./pages/AboutMe";
+import Projects from "./pages/Projects";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+//Router
+import { Switch, Route, useLocation } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className='App'>
-      <h1>Website</h1>
+      <GlobalStyle />
+      <Nav />
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path='/' exact>
+            <AboutMe />
+          </Route>
+          <Route path='/projects' exact>
+            <Projects />
+          </Route>
+        </Switch>
+      </AnimatePresence>
+      <Footer />
     </div>
   );
 }
