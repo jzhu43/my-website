2d027832935003849e1a2acb3717a91b825ec0db
Jason Zhu <jzhu43@ucmerced.edu>
2021-08-30 20:45:01 -0700

    Styles for html

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html{
+    @media(max-width: 1700px){
+        font-size: 75%;
+    }
+}
+
+body{
+    background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
+}
+
+button{
+    font-weight: bold;
+    font-size: 1.1.rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #4c72cb;
+    background: transparent;
+    color: white;
+    transition: all 0.5s ease;
+    font-family: 'Inter', sans-serif;
+    border-radius: 25px;
+    &:hover{
+        background-color: #4c72cb;
+        color: white
+    }
+}
+
+h2{
+    font-weight: lighter;
+    font-size: 4rem;
+}
+h3{
+    color: white;
+}
+h4{
+    font-weight: bold;
+}
+a{
+    font-size: 1.1.rem;
+}
+span{
+    font-weight: bold;
+    color: #4c7bcb;
+}
+p{
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+}
+
+`;
+
+export default GlobalStyle;
